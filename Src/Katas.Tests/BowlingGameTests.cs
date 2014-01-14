f7d657b1f3e249623c52102cc3ee496d0c8af533
artur-s <arturs@iqmetrix.com>
2014-01-14 08:40:29 -0600

    started

---
+++ Src/Katas.Tests/BowlingGameTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Katas.Tests
+{
+	public class BowlingGameTests
+	{
+
+		[Theory]
+		[InlineData("00000000000000000000", 0)]
+		[InlineData("11111111111111111111", 20)]
+		[InlineData("1/111111111111111111", 29)]
+		[InlineData("XXXXXXXXXXXX", 300)]
+		[InlineData("9-9-9-9-9-9-9-9-9-9-", 90)]
+		[InlineData("5/5/5/5/5/5/5/5/5/5/5", 150)]
+		public void ItHasToPass(string scores, int expected)
+		{
+			// arrange
+			var sut = new BowlingGame();
+
+			// act 
+			int actual = sut.CalculateTotal(scores);
+
+			Assert.Equal(expected, actual);
+
+		}
+
+
+	}
+}
---
+++ Src/Katas/BowlingGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+	/// <summary>
+	/// Implementation of the Bowling Kata, ref="http://codingdojo.org/cgi-bin/wiki.pl?KataBowling"
+	/// </summary>
+	public class BowlingGame
+	{
+		public int CalculateTotal(string score)
+		{
+
+			return 0;
+		}
+	}
+
+}
