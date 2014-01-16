5a75763832518125a9fdd6d88ac5cda69c368e97
artur-s <arturs@iqmetrix.com>
2014-01-16 09:30:20 -0600

    added fsharp project

---
+++ Src/BowlingGame.FSharp/BowlingGame.fs
@@ -0,0 +1,16 @@
+﻿
+// bowling kata, ref: http://codingdojo.org/cgi-bin/wiki.pl?KataBowling
+
+#if INTERACTIVE
+#r "../../packages/xunit.1.9.2/lib/net20/xunit.dll"
+#r "../../packages/FsUnit.xUnit.1.2.2.1/Lib/Net40/FsUnit.Xunit.dll"
+#r "../../packages/xunit.extensions.1.9.2/lib/net20/xunit.extensions.dll"
+#endif
+
+namespace BowlingGame.FSharp
+
+open System
+open Xunit
+open Xunit.Extensions
+open FsUnit.Xunit
+
