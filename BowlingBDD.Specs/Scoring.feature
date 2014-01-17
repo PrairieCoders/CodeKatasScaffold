72232dfadbc8464d853a4e6ac93c9b318c1c8fc8
Brad <rhodes.brad@gmail.com>
2014-01-17 02:43:38 -0600

    Initial commit

---
+++ BowlingBDD.Specs/Scoring.feature
@@ -0,0 +1,21 @@
+﻿Feature: Scoring
+	When a player enters their series of frame scores
+	I want to calculate a score for them
+
+@mytag
+Scenario: A perfect game
+	Given A bowling game
+	When The frame series is: X,X,X,X,X,X,X,X,X,X,X,X
+	Then the result should be 300
+
+Scenario: All spares game
+	Given A bowling game
+	When All the frame scores are 5 and /
+	And The bonus roll is 5
+	Then the result should be 150
+
+Scenario: No strikes or spares game
+	Given A bowling game
+	When All the frame scores are 9 and 0
+	Then the result should be 90
+
---
+++ BowlingBDD/BowlingGame.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingBDD
+{
+    public class BowlingGame
+    {
+        private const int MaxRolls = 21;
+
+        private readonly char[] _rolls = new char[MaxRolls];
+        private int _currentRoll;
+
+        public BowlingGame()
+        {
+            
+        }
+
+        public void Roll(char roll)
+        {
+            _rolls[_currentRoll] = roll;
+            _currentRoll++;
+        }
+
+        
+
+        public int GetScore()
+        {
+            var scoreStack = new Stack<int>();
+            for (var i = 0; i < MaxRolls; i++)
+            {
+                var roll = _rolls[i];
+                if (Char.IsDigit(roll))
+                {
+                    scoreStack.Push(GetIntValue(roll));
+                    continue;
+                }
+                if (roll == '-')
+                {
+                    scoreStack.Push(GetIntValue(roll));
+                    continue;
+                }
+                if (roll == '/')
+                {
+                    scoreStack.Pop();
+                    scoreStack.Push(GetIntValue(roll));
+                    scoreStack.Push(GetIntValue(_rolls[i+1]));
+                    continue;
+                }
+                if (roll == 'x' || roll == 'X')
+                {
+                    scoreStack.Push(GetIntValue(roll));
+                    scoreStack.Push(GetIntValue(_rolls[i+1]));
+                    scoreStack.Push(GetIntValue(_rolls[i+2]));
+                    continue;
+                }
+                if (roll == default(char))
+                {
+                    break;
+                }
+                throw new ArgumentException("Invalid roll.");
+            }
+
+            var total = 0;
+            while (scoreStack.Any())
+                total += scoreStack.Pop();
+
+            return total;
+        }
+
+        private int GetIntValue(char charValue)
+        {
+            if (Char.IsDigit(charValue))
+            {
+                var temp = Char.GetNumericValue(charValue);
+                var val = Convert.ToInt32(temp);
+                return val;
+            }
+            if (charValue == '-')
+                return 0;
+            if (charValue == '/')
+                return 10;
+            if (charValue == 'x' || charValue == 'X')
+                return 10;
+            if (charValue == default(char))
+                return 0;
+
+            throw new ArgumentException("Invalid roll character");
+        }
+    }
+
+   
+}
