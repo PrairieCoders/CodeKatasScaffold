7641c0e802c187aac8ccd51fed3561bc36dd187e
artur-s <arturs@iqmetrix.com>
2014-01-21 08:19:12 -0600

    added implementation

--- Src/Katas.Tests/BowlingGameTests.cs
+++ Src/Katas.Tests/BowlingGameTests.cs
@@ -11,10 +11,10 @@ namespace Katas.Tests
 		[Theory]
 		[InlineData("00000000000000000000", 0)]
 		[InlineData("11111111111111111111", 20)]
+		[InlineData("5/5/5/5/5/5/5/5/5/5/5", 150)]
 		[InlineData("1/111111111111111111", 29)]
 		[InlineData("XXXXXXXXXXXX", 300)]
 		[InlineData("9-9-9-9-9-9-9-9-9-9-", 90)]
-		[InlineData("5/5/5/5/5/5/5/5/5/5/5", 150)]
 		public void ItShouldCalculateTotalScoreCorrectly(string scores, int expected)
 		{
 			// arrange
--- Src/Katas/BowlingGame.cs
+++ Src/Katas/BowlingGame.cs
@@ -1,89 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Katas
 {
 	/// <summary>
-	/// Implementation of the Bowling Kata, ref="http://codingdojo.org/cgi-bin/wiki.pl?KataBowling",
-	/// HOW? : 
-	///		- visitor pattern ?,
-	///		- monoids and map-reduce?
+	/// My implementation of the Bowling Kata, ref="http://codingdojo.org/cgi-bin/wiki.pl?KataBowling",
 	/// </summary>
 	public class BowlingGame
 	{
-		public int CalculateTotal(string gameLog)
+
+		public int CalculateTotal(string gameLine)
 		{
-
-			gameLog
-				.Select( c => c == 'X' ? new []{'X','_'} : new[]{'X'})
-				// add dummy frame for strike (X)
-				.SelectMany( @throw => )
-
-				.Zip(gameLog.Skip(1), (f,s) => new Frame {First})
-
-			return 0;
-		}
-	}
-
-	class Frame
-	{
-		public Score FirstThrow { get; private set; }
-		public Score SecondThrow { get; private set; }
-
-		protected Frame(char firstThrow, char secondThrow)
-		{
-			FirstThrow = Score.Create(firstThrow);
-			SecondThrow = Score.Create(secondThrow);
-		}
-	}
-
-	abstract class Score
-	{
-		readonly char _throw;
-
-		private Score(char @throw)
-		{
-			_throw = @throw;
+			return
+				gameLine
+					.Reverse()
+					.Aggregate((IRoll)new NullRoll(), (next, r) => new Roll(r, next))
+					.GetTotal();
 		}
 
-		public Throw Throw { get; set; }
-
-		public static Score Create(char @throw)
+		interface IRoll
 		{
-			return ToThrow(@throw);
+			IRoll Next { get; }
+			int Points { get; }
+			int GetTotal();
+			bool IsStrike { get; }
+			bool IsSpare { get; }
+			bool IsHit { get; }
 		}
 
-		static Score ToThrow(char @throw)
+		class Roll : IRoll
 		{
-			switch (@throw)
+			private readonly char _roll;
+			public IRoll Next { get; private set; }
+			public int Points { get { return ToPoints(_roll); } }
+			public bool IsStrike { get { return _roll == 'X'; } }
+			public bool IsSpare { get { return _roll == '/'; } }
+			public bool IsHit { get { return Char.IsDigit(_roll); } }
+
+			public Roll(char roll, IRoll next = null)
 			{
-				case 'X':
-					return new Strike();
-				case '/':
-					return new Spare();
+				_roll = roll;
+				Next = next ?? new NullRoll();
 			}
-			return __;
+
+			private static int ToPoints(char c)
+			{
+				return c == '-' ? 0 :
+				c == 'X' || c == 'X' || c == '/' ? 10 :
+				c - '0';
+			}
+
+			int GetPoints()
+			{
+				if (IsHit && Next.IsSpare)
+					return 0;
+				if (IsStrike && (IsEmpty(Next) || IsEmpty(Next.Next)))
+					return 0;
+				if (IsSpare)
+					return Points + (IsEmpty(Next.Next) ? 0 : Next.Points);
+				if (IsStrike)
+					return Points + Next.Points + Next.Next.Points;
+
+				return Points;
+			}
+
+			private static bool IsEmpty(IRoll next)
+			{
+				return !(next.IsHit || next.IsSpare || next.IsStrike);
+			}
+
+			public int GetTotal()
+			{
+				return GetPoints() + Next.GetTotal();
+			}
+		}
+
+		class NullRoll : IRoll
+		{
+			public IRoll Next { get { return new NullRoll(); } }
+			public int Points { get { return 0; } }
+			public int GetTotal() { return 0; }
+			public bool IsStrike { get { return false; } }
+			public bool IsSpare { get { return false; } }
+			public bool IsHit { get { return false; } }
 		}
 	}
 
-	class Spare : Score
-	{
-	}
-
-	class Strike : Score
-	{
-	}
-
-
-	enum Throw
-	{
-		Strike,
-		Spare,
-		Hit,
-		Miss,
-		//None
-	}
 }
+
