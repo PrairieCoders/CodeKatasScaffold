90cc6907a0eda010220824eba2ac54f2b6f3c4e2
ChristopherWolf <christopherw@iqmetrix.com>
2014-01-21 10:34:48 -0600

    Started kata

---
+++ Src/Kata.Specs/StringCalculatorSpecs.cs
@@ -0,0 +1,51 @@
+﻿using Katas;
+using Machine.Specifications;
+
+namespace Kata.Specs
+{
+	public abstract class StringCalculatorSpecs
+	{
+		protected static StringCalculator calculator;
+		protected static int result;
+
+		// The Establish delegate is the "Arrange" part of the spec class. 
+		// The establish will only run once, so your assertions should not mutate any state or you may be in trouble.
+		Establish context = () => calculator = new StringCalculator();
+	}
+
+	[Subject(typeof(StringCalculator), "Adding numbers")]
+	public class When_adding_a_string_of_numbers_that_is_empty : StringCalculatorSpecs
+	{
+		// The Because delegate is the "Act" part of the spec class. 
+		// It should be the single action for this context, the only part that mutates state, against which all of the assertions can be made. 
+		Because of = () => result = calculator.Add("");
+
+		// The It delegate is the "Assert" part of the spec class. It may appear one or more times in your spec class. 
+		// Each statement should contain a single assertion, so that the intent and failure reporting is crystal clear.
+		It should_return_0 = () => result.ShouldEqual(0);
+	}
+
+	[Subject(typeof(StringCalculator), "Adding numbers")]
+	public class When_adding_a_string_of_numbers_that_has_only_one_number : StringCalculatorSpecs
+	{
+		Establish context = () => inputNumbers = "99";
+
+		Because of = () => result = calculator.Add(inputNumbers);
+
+		It should_return_the_number = () => result.ShouldEqual(int.Parse(inputNumbers));
+
+		static string inputNumbers;
+	}
+
+//	[Subject(typeof(StringCalculator), "Adding numbers")]
+//	public class When_adding_a_string_of_numbers_that_has_two_numbers : StringCalculatorSpecs
+//	{
+//		Establish context = () => inputNumbers = "99";
+//
+//		Because of = () => result = calculator.Add(inputNumbers);
+//
+//		It should_return_the_number = () => result.ShouldEqual(int.Parse(inputNumbers));
+//
+//		static string inputNumbers;
+//	}
+}
---
+++ Src/Katas/StringCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Katas
+{
+	public class StringCalculator
+	{
+		public int Add(string numbers)
+		{
+			return numbers.Length == 0 ? 0 : int.Parse(numbers)-2;
+		}
+	}
+}
