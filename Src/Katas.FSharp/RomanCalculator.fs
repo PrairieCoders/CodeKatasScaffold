f205626accf2d442e53bfadf3a70c3362607df4e
artur-s <arturs@iqmetrix.com>
2014-02-07 08:18:38 -0600

    more reduction rules

--- Src/Katas.FSharp/RomanCalculator.fs
+++ Src/Katas.FSharp/RomanCalculator.fs
@@ -4,7 +4,7 @@ module RomanCalculator =
 
     
     [<Literal>]
-    let tokens = "IVXLCDM"
+    let tokens = "IVXLCDM" // "MDCLXVI" 
 
     type Roman = Roman of string
     
@@ -14,36 +14,40 @@ module RomanCalculator =
 // requirements:
     // order: M, D, C, L, 
     // max one for: V, L, D
-    // max four repetitions for: I, X, C, M
+    // max three repetitions for: I, X, C, M
     
-    // substitutions:
+    // reduction rules:
     // a) IIII=>IV,
     // b) VIIII=>IX,
     // c) XXXX=>XL,
     // d) LXXXX=>XC,
     // e) CCCC=>CD,
     // f) DCCCC=>CM
+    // i.e.:
+    //    (4x odd token -> 1x odd and next) 
+    //    (1x even and 4x odd token -> 1x the odd and next of the even) 
+    // 
+
+    // additional reduction rules:
+    // g-i) IIIII -> V, XXXXX -> L, CCCCC -> D
+    // j-l) VV -> X, LL -> C, DD -> M
+    // i.e.:
+    //    4x odd -> 1x next odd
+    //    2x even
 
     // samples:
-        // 1066 = MLXVI,
-        // 207 = CCVII,
-        // 2014 = MMXIV
-        // III + II = IIIII
-    // 2014 = MMXIV before reduction:
-    // MMXIIII
+        // XLIV + XXIX = LXXIII
+        // XXVIII + LXVI = XCIV
 
     // process:
-    //  1) enlarge arguments, e.g.: MMXIV becomes MMXIIII
+    //  1) expand arguments, e.g.: MMXIV becomes MMXIIII
     //  2) concatenate tokens
     //  3) sort (higher first)
-    //  4) reduce (somehow)
+    //  4) reduce (left to right, until nothing to reduce)
     //
     // sample: XLIV + XXIX  = LXXIII (44 + 29 = 73)
-    //  1. enlarge: XXXXIIII + XXVIIII
+    //  1. expand: XXXXIIII + XXVIIII
     //  2. concatenate: XXXXIIIIXXVIIII
     //  3. sort: XXXXXXVIIIIIIII
     //  4. reduce: one level rule at the time (reducing from left to right, higher rule first)
-    //      - rule "c": XXXXXXVIIIIIIII -> XLXXVIIIIIIII
-    //      - rule "b": XLXXVIIIIIIII -> XLXXIXIIII
-    //      - rule "a": XLXXIXIIII -> XLXXIXIV      <- this is 73 but not really correct form, hmm
             
