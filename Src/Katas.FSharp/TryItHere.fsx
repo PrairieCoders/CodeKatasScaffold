103e36a8bcc42193691157dd9e7192741d5329c5
artur-s <arturs@iqmetrix.com>
2014-04-03 09:24:45 -0600

    started designing types for the mars rover control

--- Src/Katas.FSharp/TryItHere.fsx
+++ Src/Katas.FSharp/TryItHere.fsx
@@ -1,7 +1,12 @@
 ﻿// See why F# at http://fsharpforfunandprofit.com/why-use-fsharp/
 
-#load "CurrentKata.fs"
-open Katas.FSharp
+#load "MarsKata.fs"
+open Katas.FSharp.Mars
 
 // Try your kata here using F# interactive window
 
+
+let roverAtMars = landAt 10 10 North
+
+// fail
+landAt 200 200 North |> ignore
